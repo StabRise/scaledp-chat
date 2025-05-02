90b603243c056ce4c6e62b75c6477ff543ef87eb
mykola <mykola.melnyk.ml@gmail.com>
2025-05-02 19:57:48 +0200

    Disable taskiq

--- scaledp_chat/web/api/router.py
+++ scaledp_chat/web/api/router.py
@@ -1,5 +1,6 @@
 from fastapi.routing import APIRouter
 
+from scaledp_chat.settings import settings
 from scaledp_chat.web.api import chat, docs, dummy, echo, monitoring, rabbit
 
 api_router = APIRouter()
@@ -7,5 +8,8 @@ api_router.include_router(monitoring.router)
 api_router.include_router(docs.router)
 api_router.include_router(echo.router, prefix="/echo", tags=["echo"])
 api_router.include_router(dummy.router, prefix="/dummy", tags=["dummy"])
-api_router.include_router(rabbit.router, prefix="/rabbit", tags=["rabbit"])
+
 api_router.include_router(chat.router, prefix="/chat", tags=["chat"])
+
+if settings.with_taskiq:
+    api_router.include_router(rabbit.router, prefix="/rabbit", tags=["rabbit"])
